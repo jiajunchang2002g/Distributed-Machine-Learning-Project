7af19f2a7082093a4088c6a4716ec426e2b6543e
Chang Jia Jun <jiajunchang2002@gmail.com>
2025-11-03 03:16:22 +0800

    fix: bcast params to each node

--- engine.cpp
+++ engine.cpp
@@ -23,25 +23,37 @@ double computeDistance(double *a, double *b, int dim)
 void Engine::KNN(Params &p, vector<DataPoint> &dataset, vector<Query> &queries)
 {
         int numtasks, rank, dest, src, rc, count, tag = 1;
-        int sendcount, recvcount;
+        int num_attrs, num_queries, num_data, sendcount, recvcount;
         MPI_Comm comm = MPI_COMM_WORLD;
         MPI_Status Stat;
         MPI_Comm_size(comm, &numtasks);
         MPI_Comm_rank(comm, &rank);
 
-        MPI_Bcast(&p, sizeof(Params), MPI_BYTE, 0, comm);
-        sendcount = p.num_data / numtasks;
-        recvcount = p.num_data / numtasks;
+        if (rank == 0)
+        {
+                num_attrs = p.num_attrs;
+                num_queries = p.num_queries;
+                num_data = p.num_data;
+        }
+        MPI_Bcast(&num_attrs, 1, MPI_INT, 0, comm);
+        MPI_Bcast(&num_queries, 1, MPI_INT, 0, comm);
+        MPI_Bcast(&num_data, 1, MPI_INT, 0, comm);
+
+        sendcount = num_data / numtasks;
+        recvcount = num_data / numtasks;
+
+        cout << "Rank " << rank << " will sendcount: " << sendcount << ", recvcount: " << recvcount << endl;
+        cout.flush();
         
         // recvbuffer(s)
         double **attrs_rx = (double **)malloc(sizeof(double *) * recvcount);
         for (int i = 0; i < recvcount; i++)
         {
-                attrs_rx[i] = (double *)malloc(sizeof(double) * p.num_attrs);
+                attrs_rx[i] = (double *)malloc(sizeof(double) * num_attrs);
         }
         int *id_rx = (int *)malloc(sizeof(int) * recvcount);
         int *label_rx = (int *)malloc(sizeof(int) * recvcount);
-        double **q_attrs_rx = (double **)malloc(sizeof(double *) * p.num_attrs);
+        double **q_attrs_rx = (double **)malloc(sizeof(double *) * num_attrs);
 
         // sendbuffers(s)
         double **attrs_tx;
@@ -52,7 +64,7 @@ void Engine::KNN(Params &p, vector<DataPoint> &dataset, vector<Query> &queries)
                 attrs_tx = (double **)malloc(sizeof(double *) * sendcount);
                 for (int i = 0; i < sendcount; i++)
                 {
-                        attrs_tx[i] = (double *)malloc(sizeof(double) * p.num_attrs);
+                        attrs_tx[i] = (double *)malloc(sizeof(double) * num_attrs);
                 }
                 id_tx = (int *)malloc(sizeof(int) * sendcount);
                 label_tx = (int *)malloc(sizeof(int) * sendcount);
@@ -66,13 +78,13 @@ void Engine::KNN(Params &p, vector<DataPoint> &dataset, vector<Query> &queries)
                 }
         }
 
-        std::cout << "Rank " << rank << " starting KNN with " << p.num_queries << " queries." << endl;
+        std::cout << "Rank " << rank << " starting KNN with " << num_queries << " queries." << endl;
         std::cout.flush();
-        for (int i = 0; i < p.num_queries; i++)
+        for (int i = 0; i < num_queries; i++)
         {
                 int query_id;
                 int query_k;
-                double *query_attrs = (double *)malloc(sizeof(double) * p.num_attrs);
+                double *query_attrs = (double *)malloc(sizeof(double) * num_attrs);
                 if (rank == 0)
                 {
                         query_id = queries[i].id;
@@ -81,17 +93,17 @@ void Engine::KNN(Params &p, vector<DataPoint> &dataset, vector<Query> &queries)
                 }
                 MPI_Bcast(&query_id, 1, MPI_INT, 0, comm);
                 MPI_Bcast(&query_k, 1, MPI_INT, 0, comm);
-                MPI_Bcast(query_attrs, p.num_attrs, MPI_DOUBLE, 0, comm);
+                MPI_Bcast(query_attrs, num_attrs, MPI_DOUBLE, 0, comm);
 
                 MPI_Scatter(id_tx, sendcount, MPI_INT, id_rx, recvcount, MPI_INT, 0, comm);
                 MPI_Scatter(label_tx, sendcount, MPI_INT, label_rx, recvcount, MPI_INT, 0, comm);
-                MPI_Scatter(attrs_tx[0], sendcount * p.num_attrs, MPI_DOUBLE,
-                            attrs_rx[0], recvcount * p.num_attrs, MPI_DOUBLE, 0, comm);
+                MPI_Scatter(attrs_tx[0], sendcount * num_attrs, MPI_DOUBLE,
+                            attrs_rx[0], recvcount * num_attrs, MPI_DOUBLE, 0, comm);
 
                 std::vector<std::tuple<double, int, int>> local_results; // distance, label, id
                 for (int j = 0; j < recvcount; j++)
                 {
-                        double dist = computeDistance(query_attrs, attrs_rx[j], p.num_attrs);
+                        double dist = computeDistance(query_attrs, attrs_rx[j], num_attrs);
                         local_results.push_back(std::make_tuple(dist, label_rx[j], id_rx[j]));
                 }
                 std::sort(local_results.begin(), local_results.end());
